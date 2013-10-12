bc6185fc2f8617348dad156ec1f14b6cc14c20ec
Thomas Dimson <tdimson@gmail.com>
2013-10-12 15:07:00 -0700

    Debug macros, lda reader

--- src/lda/core.clj
+++ src/lda/core.clj
@@ -12,6 +12,9 @@
 (import 'com.thomasdimson.wikipedia.Data$DumpPage)
 (import 'com.thomasdimson.wikipedia.Data$WikiGraphNode)
 
+; Macro zone
+(defmacro dbg-b [str & body] `(do (println ~str) ~@body))
+(defmacro dbg-a [str & body] `(let [x# ~@body] (do (println ~str) x#)))
 
 ; Make graph out of wikipedia
 (def WikiGraphNode (protodef Data$WikiGraphNode))
@@ -54,21 +57,38 @@
 
 (defn read-wiki-graph-nodes [^String input-file] (protobuf-seq WikiGraphNode input-file))
 
-(defn make-intermediate-tspr-nodes [^String input-file ^String lda-file]
-  (let [lda (into-array Double/TYPE (map double (range 100)))]
+(defn make-intermediate-tspr-nodes [^String input-file lda-map]
     (map-indexed
       (fn [idx graph-node]
         (IntermediateTSPRNode. idx
-          (int (:id graph-node)) (:title graph-node) (into-array Long/TYPE (map long (:edges graph-node))) lda
+          (int (:id graph-node)) (:title graph-node) (into-array Long/TYPE (map long (:edges graph-node))) (get lda-map (:title graph-node))
         )
       )
       (read-wiki-graph-nodes input-file)
-     )
+  )
+)
+
+(defn read-lda-map [^String lda-file]
+  (let [line2assoc
+          (fn [^String line]
+            (let  [split (string/split line #"\t")
+                   sequential-id (get split 0)
+                   title (get split 1)
+                   topics (map #(Integer/parseInt %) (take-nth 2 (drop 2 split)))
+                   probs (map #(Double/parseDouble %) (take-nth 2 (drop 3 split)))
+                   ]
+            [title (into-array Double/TYPE (map #(get % 1) (sort (cons [100000 1.0] (map vector topics probs)))))]
+              )
+            )]
+      (with-open [r (io/reader lda-file)] (into {} (pmap line2assoc (rest (line-seq r)))))
   )
 )
 
 (defn tspr [^String input-file ^String lda-file ^String output-file]
-  (let [intermediate-vector (java.util.ArrayList. (make-intermediate-tspr-nodes input-file lda-file))]
+  (let [
+         lda-map (dbg-b "Reading lda map" (read-lda-map lda-file))
+         intermediate-vector (dbg-b "Reading intermediate nodes" (java.util.ArrayList. (make-intermediate-tspr-nodes input-file lda-map)))
+       ]
     (do
       (TopicSensitivePageRank/rankInPlace intermediate-vector)
       (with-open [w (io/writer output-file)]
