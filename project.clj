4a987db49955b8dcd21afd10d6e8f7255b420f8f
Thomas Dimson <tdimson@gmail.com>
2013-10-18 22:23:53 -0700

    Fix project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,5 @@
-(defproject lda "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject lda "0.1.0"
+  :description "Latent Dirichlet Allocation + Topic Sensitive PageRank for Wikipedia"
   :plugins [[lein-protobuf "0.3.1"]]
   :java-source-paths ["src/java"]
   :dependencies [
